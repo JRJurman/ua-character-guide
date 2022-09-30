00774238ca70f1a36196147dfa7d89add4c7cd88
JRJurman <j.r.jurman@gmail.com>
2022-09-30 14:10:09 -0400

    Adding race pages and control
    Co-authored-by: Tina Howard <chtinahow@users.noreply.github.com>

--- src/page/page.ts
+++ src/page/page.ts
@@ -1,16 +1,21 @@
 import { registerHtml, TramOneComponent } from "tram-one";
 import "./expanded-page.css";
 import "./page-colors.css";
+import "./page.css";
 
 const html = registerHtml();
 
-const page: TramOneComponent<{ title: string; selection: string }> = (
-  { title, selection },
-  children
-) => {
+const page: TramOneComponent<{
+  title: string;
+  selection: string;
+  source: string;
+}> = ({ title, selection, source }, children) => {
   return html`
     <section class="page" page-color="grey" page-selection=${selection}>
-      <h2>${title}</h2>
+      <h2 class="header">
+        <span class="title">${title}</span>
+        <span class="source">${source}</span>
+      </h2>
       ${children}
     </section>
   `;
